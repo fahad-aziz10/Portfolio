e4e36422452f4da6c97aa855f8d9a3aa8f928bf0
fahad-aziz10 <110331761+fahad-aziz10@users.noreply.github.com>
2023-11-07 07:31:13 +0000

    Name configuration in template

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Fahad Aziz Khan",
+  title: "Electrical Engineer and Machine Learning Expert",
+  email: "fahad_aziz10@yahoo.com",
+  gitHub: "github.com/fahad-aziz10",
+  instagram: "",
+  linkedIn: "fahad-aziz10",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
